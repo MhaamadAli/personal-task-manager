4962750a57c00eeab1fd6b348ae55e78461a5cb7
MhaamadAli <m.alialaawar@gmail.com>
2024-04-15 11:14:29 +0300

    created boards routes

---
+++ server/routes/boards.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const isAuthenticated = require("../middlewares/auth.middleware");
+const boardController = require("../controllers/boards.controller");
+const router = express.Router();
+
+router.get("/", isAuthenticated, boardController.getAllBoards);
+router.get("/:id", isAuthenticated, boardController.getBoard);
+router.post("/", isAuthenticated, boardController.addBoard);
+router.put("/:id", isAuthenticated, boardController.editBoard);
+router.post("/delete/", isAuthenticated, boardController.deleteBoard);
+
+
+module.exports = router;
