870ef0796a43b3cc113362744a50b9ccd7e79b0d
MhaamadAli <m.alialaawar@gmail.com>
2024-04-17 00:05:53 +0300

    components: updated  Column component

--- frontend/src/pages/components/Column.jsx
+++ frontend/src/pages/components/Column.jsx
@@ -1,6 +1,46 @@
-const Column = () => {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import boardsSlice from "../../redux/boardsSlice";
+import Task from "./Task";
+const Column = ({ colIndex }) => {
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const board = boards.find((board) => board.isActive === true);
+  const col = board.columns.find((col, i) => i === colIndex);
+  useEffect(() => {}, [dispatch]);
+
+  const handleOnDrop = (e) => {
+    const { prevColIndex, taskIndex } = JSON.parse(
+      e.dataTransfer.getData("text")
+    );
+
+    if (colIndex !== prevColIndex) {
+      dispatch(
+        boardsSlice.actions.dragTask({ colIndex, prevColIndex, taskIndex })
+      );
+    }
+  };
+
+  const handleOnDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Column</div>
-  )
-}
-export default Column
+    <div
+      onDrop={handleOnDrop}
+      onDragOver={handleOnDragOver}
+      className="scrollbar-hide   mx-5 pt-[90px] min-w-[280px] "
+    >
+      <p className=" font-semibold flex  items-center  gap-2 tracking-widest md:tracking-[.2em] text-[#828fa3]">
+        <div className={`rounded-full w-4 h-4  `} />
+        {col.title} ({col.tasks.length})
+      </p>
+
+      {col.tasks.map((task, index) => (
+        <Task key={index} taskIndex={index} colIndex={colIndex} />
+      ))}
+    </div>
+  );
+};
+
+export default Column;
--- frontend/src/pages/components/Home.jsx
+++ frontend/src/pages/components/Home.jsx
@@ -1,6 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AddEditBoard from "./AddEditBoard";
+import Sidebar from "./Sidebar";
+import sendAuthRequest from "../../core/tools/authRequest";
+import { requestMethods } from "../../core/requests/requestMethod";
+import { loadBoards } from "../../redux/boardsSlice";
+import Column from "./Column";
+import Header from "./Header";
 const Home = () => {
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const boards = useSelector((state) => state.boards);
+  const board = boards?.find((board) => board.isActive === true);
+  const columns = board?.columns;
+  const getBoards = () => {
+    sendAuthRequest(requestMethods.GET, "boards").then((response) => {
+      console.log(response.data);
+      const boardsData = response.data;
+      dispatch(loadBoards(boardsData));
+    });
+  };
+  useEffect(() => {
+    getBoards();
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <div className=" bg-[#f4f7fd]  scrollbar-hide h-screen flex dark:bg-[#20212c]  overflow-x-scroll gap-6  ml-[261px]">
+      <Sidebar
+        setIsBoardModalOpen={setIsBoardModalOpen}
+        isBoardModalOpen={isBoardModalOpen}
+      />
+      <Header />
+      {columns?.length > 0 && (
+        <>
+          {columns?.map((col, index) => (
+            <Column key={index} colIndex={index} />
+          ))}
+          <div
+            onClick={() => {
+              setIsBoardModalOpen(true);
+            }}
+            className=" h-screen  flex justify-center items-center font-bold text-2xl hover:text-[#635FC7] transition duration-300 cursor-pointer bg-[#E9EFFA] scrollbar-hide mb-2   mx-5 pt-[90px] min-w-[280px] text-[#828FA3] mt-[135px] rounded-lg "
+          >
+            + New Column
+          </div>
+        </>
+      )}
+      {isBoardModalOpen && (
+        <AddEditBoard
+          type="edit"
+          reloadBoards={getBoards}
+          setIsBoardModalOpen={setIsBoardModalOpen}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Home;
