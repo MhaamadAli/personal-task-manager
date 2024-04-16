9e09517e1a4295f19575f6b3a565df587dfb619c
MhaamadAli <m.alialaawar@gmail.com>
2024-04-16 23:38:07 +0300

    App.jsx: added login signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/components/Home";
-function App() {
+const App = () => {
+  const token = localStorage.getItem("token");
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
         </Routes>
+        {token && (
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        )}
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
